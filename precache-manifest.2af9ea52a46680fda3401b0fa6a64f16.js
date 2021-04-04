c9775fa401a89762e7bdd0fbaddef271202d6f73
davisnguyen <anhri89@yahoo.com>
2021-04-04 10:15:02 -0500

    Updates

---
+++ precache-manifest.2af9ea52a46680fda3401b0fa6a64f16.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "403d45f70839a130ccfe43e425db225a",
+    "url": "/todo-app-v2/index.html"
+  },
+  {
+    "revision": "2d7ad5498870dad2ee10",
+    "url": "/todo-app-v2/static/js/2.c0e13367.chunk.js"
+  },
+  {
+    "revision": "d16fd50640a1c3ca3374",
+    "url": "/todo-app-v2/static/js/main.e7251bac.chunk.js"
+  },
+  {
+    "revision": "59b3cd6de7f3d465f76d",
+    "url": "/todo-app-v2/static/js/runtime-main.4d85fd71.js"
+  }
+]);
